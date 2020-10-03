9c8761010b92e0fbe9adbc5dbe3f068bf1a7bdea
Aline Pessoa da Silva Kaiper <lilica.kaiper@gmail.com>
2020-10-03 14:57:53 -0300

    Para juntar com o Hique

---
+++ LD47/Assets/Scripts/LeitorDeTxt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LeitorDeTxt
+{
+    string[] dialogos;
+
+    public void LerTxt ()
+    {
+        StreamReader leitor = new StreamReader( "Assets/Resources/Dialogos.txt");
+        dialogos = leitor.ReadToEnd().Split('#');
+    }
+
+    public string RetornaDialogo(int personagem)
+    {
+        return dialogos[personagem];
+    }
+}
